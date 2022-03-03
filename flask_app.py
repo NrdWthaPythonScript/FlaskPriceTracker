d07c1016afc787d779ea581992d730a6180d2bc3
stephen sloper <stephen.sloper@yahoo.com>
2022-03-02 16:43:30 -0800

    fixed typo

--- flask_app.py
+++ flask_app.py
@@ -15,7 +15,7 @@ def home():
 def about():
     return "<h1>About Page<h1>"
 
-@app.route("/register" methods=['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     return render_template('register.html', form=form)
