62e713594c3b424318d6aeb4eadfd69a03bde756
stephen sloper <stephen.sloper@yahoo.com>
2022-03-03 22:13:53 -0800

    login form validation and user verification

--- flask_app.py
+++ flask_app.py
@@ -26,9 +26,11 @@ def register():
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.email.data == 'stephen.sloper@yahoo.com' and form.password.data == 'password':
         flash(f'{form.username.data} successfully signed in!', 'success')
         return redirect(url_for('home'))
+    else:
+        flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', form=form)
 
 if __name__ == '__main__':
