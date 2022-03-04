8eb626ad9896c228aca1343e3b60d96cb461a90c
stephen sloper <stephen.sloper@yahoo.com>
2022-03-03 21:43:29 -0800

    added missing if statement for login function

--- flask_app.py
+++ flask_app.py
@@ -26,6 +26,9 @@ def register():
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'{form.username.data} successfully signed in!', 'success')
+        return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
 if __name__ == '__main__':
